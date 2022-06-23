89b18aae44b8f62668b466a5cd8a10cc10c7ad4e
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-23 09:40:30 -0600

    Add author to book cards

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -3,12 +3,15 @@ import { Book } from "../types/book"
 import { Availability } from "./Availability"
 import Genres from "./Genres"
 
-export function BookCard({ title, isCheckedOut, genres, _id }: Book) {
+export function BookCard({ title, isCheckedOut, genres, _id, author }: Book) {
   return (
-    <div className="grid md:grid-cols-2 px-4">
-      <div className="bg-white shadow-md mb-2 p-4 rounded-xl">
+    <div className="grid px-4">
+      <div className="bg-white shadow-md mb-4 p-4 rounded-xl">
         <Link to={`/books/${_id}`}>
-          <p className="font-bold text-xl mb-2">{title}</p>
+          <div className="mb-3">
+            <p className="font-bold text-xl">{title}</p>
+            <p className="ml-2">{author}</p>
+          </div>
           <div className="flex flex-wrap gap-1 items-center ml-2">
             <Availability isCheckedOut={isCheckedOut} />
             <Genres genres={genres} />
